a20a804e578fe0fc5b7b0f8dd5d9223394df0944
Reilly Shofner <reilly.shofner@gmail.com>
2024-02-22 13:54:02 -0800

    complete phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 12;
+let num4 = 3;
+
+const mod = num3 / num4;
+console.log(mod);
+
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
+console.log(max);
